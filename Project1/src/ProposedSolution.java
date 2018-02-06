a70108275bdd03c501a95a03baceb81b289e8543
Martin Stigen <martin.stigen@live.no>
2018-02-06 16:01:20 +0100

    Added cars to ProposedSolution

--- Project1/src/ProposedSolution.java
+++ Project1/src/ProposedSolution.java
@@ -4,14 +4,19 @@ public class ProposedSolution {
     final int customerScore;
     final double fitnessScore;
     final Depot[] depots;
+    final Car[] cars;
 
     public ProposedSolution(Depot[] depots, int customerScore) {
         this.customerScore = customerScore;
         this.depots = depots;
+        this.cars = new Car[depots.length * depots[0].getCars().length];
+
+        int count = 0;
 
         double duration = 0;
         for (Depot depot : depots) {
             for (Car car : depot.getCars()) {
+                cars[count ++] = car;
                 duration += car.getCurrentDuration();
             }
         }
