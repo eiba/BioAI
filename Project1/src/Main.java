e00c9e4031465634df9639d033429351afd5ac8d
Martin Stigen <martin.stigen@live.no>
2018-02-21 11:27:00 +0100

    test data

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -62,7 +62,7 @@ public class Main extends Application{
         // Create a new menu for selecting the data sets
         final ChoiceBox<String> taskMenu = new ChoiceBox<>(FXCollections.observableArrayList("p01", "p02", "p03", "p04", "p05", "p06", "p07", "p08", "p09",
                 "p10", "p11", "p12", "p13", "p14", "p15", "p16", "p17", "p18", "p19",
-                "p20", "p21", "p22", "p23"));
+                "p20", "p21", "p22", "p23", "1", "2", "3"));
 
         final HashMap<String, Double> optimalMap = new HashMap<>();
         optimalMap.put("p01", 576.87);
