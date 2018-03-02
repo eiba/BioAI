88925effd233d2b92a4726894784828be3a6346e
Eirik Baug <eirikbaug@hotmail.com>
2018-03-02 15:28:37 +0100

    Added domination check to solution

--- Project2/src/Solution.java
+++ Project2/src/Solution.java
@@ -19,4 +19,18 @@ public class Solution {
         this.score = (overAlldeviationWeight*overallDeviation) - (edgeWeight*edgeValue);
     }
 
+    //check if this solution is dominated by another
+    public boolean isDominatedBy(Solution solution){
+        //solution is the solution to check if it is dominating this solution
+
+        if(this.overallDeviation > solution.overallDeviation && this.edgeValue <= solution.edgeValue){
+            return true;
+        }else if(this.overallDeviation >= solution.overallDeviation && this.edgeValue < solution.edgeValue){
+            return true;
+        }
+
+        //This solution is not dominated
+        return false;
+    }
+
 }
